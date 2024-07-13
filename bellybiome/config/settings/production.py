207518cd15f32297befd31312ec9d6326dc00c98
Chrismanati <christianwondirad1213@gmail.com>
2024-07-13 13:27:57 -0600

    misc commit..

--- bellybiome/config/settings/production.py
+++ bellybiome/config/settings/production.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "bellybiome-env.eba-46pp858m.us-east-1.elasticbeanstalk.com",
     "belly-biome.com",
+    "www.belly-biome.com",
 ]
 
 # DATABASES
@@ -61,7 +62,7 @@ CSRF_COOKIE_NAME = "__Secure-csrftoken"
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_SECONDS = 31536000
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
     "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",
