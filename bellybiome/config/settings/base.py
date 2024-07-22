97aae63a1595dec26d993571faf2e66f158172df
Chrismanati <christianwondirad1213@gmail.com>
2024-07-22 00:57:45 -0600

    misc commit..

--- bellybiome/config/settings/base.py
+++ bellybiome/config/settings/base.py
@@ -10,6 +10,11 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = BASE_DIR / "bellybiome"
 env = environ.Env()
 
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+if READ_DOT_ENV_FILE:
+    # OS environment variables take precedence over variables from .env
+    env.read_env(str(BASE_DIR / ".env"))
+
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
