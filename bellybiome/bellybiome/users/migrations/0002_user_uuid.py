f4a0c2f301906495e8b9f12607c7bb9639ef40bc
Chrismanati <christianwondirad1213@gmail.com>
2024-07-25 23:34:46 -0600

    Add UUID migrations

--- bellybiome/bellybiome/users/migrations/0002_user_uuid.py
+++ bellybiome/bellybiome/users/migrations/0002_user_uuid.py
@@ -3,13 +3,11 @@
 from django.db import migrations, models
 import uuid
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ("users", "0001_initial"),
     ]
-
     operations = [
         migrations.AddField(
             model_name="user",
